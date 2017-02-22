b931cf03eefb971609ed80c2a69725c19302bd25
Jeff <jeffrey.pierson@gmail.com>
2017-02-21 23:28:27 -0500

    Initial proof of concept

---
+++ src/TestCore/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var list = new[]
+        {
+            new { Username = "Falcore", Score = 1293, },
+            new { Username = "Dunsby", Score = 2342, },
+            new { Username = "Habisham", Score = 5234, },
+        };
+
+        var markdownTable = list.ToMarkdownTable();
+
+        Console.WriteLine(markdownTable);
+    }
+}
