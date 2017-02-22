021fb747aab606bfc9a4f310b43ce32c08a82e72
Jeff <jeffrey.pierson@gmail.com>
2017-02-22 00:51:24 -0500

    Include fields in addition to properties in output

--- src/ToMarkdownTable/LinqMarkdownTableExtensions.cs
+++ src/ToMarkdownTable/LinqMarkdownTableExtensions.cs
@@ -9,19 +9,27 @@ namespace System.Linq
         public static string ToMarkdownTable<T>(this IEnumerable<T> source)
         {
             var properties = typeof(T).GetRuntimeProperties();
+            var fields = typeof(T)
+                .GetRuntimeFields()
+                .Where(f => f.IsPublic);
+
+            var gettables = Enumerable.Union(
+                properties.Select(p => new { p.Name, GetValue = (Func<object, object>)p.GetValue, Type = p.PropertyType }),
+                fields.Select(p => new { p.Name, GetValue = (Func<object, object>)p.GetValue, Type = p.FieldType }))
+                .OrderBy(g => g.Name);
 
             var maxColumnValues = source
-                .Select(x => properties.Select(p => p.GetValue(x)?.ToString()?.Length ?? 0))
-                .Union(new[] { properties.Select(p => p.Name.Length) }) // Include header in column sizes
+                .Select(x => gettables.Select(p => p.GetValue(x)?.ToString()?.Length ?? 0))
+                .Union(new[] { gettables.Select(p => p.Name.Length) }) // Include header in column sizes
                 .Aggregate(
-                    new int[properties.Count()].AsEnumerable(),
+                    new int[gettables.Count()].AsEnumerable(),
                     (accumulate, x) => accumulate.Zip(x, Math.Max))
                 .ToArray();
 
-            var columnNames = properties.Select(p => p.Name);
+            var columnNames = gettables.Select(p => p.Name);
 
             var headerLine = "| " + string.Join(" | ", columnNames.Select((n, i) => n.PadRight(maxColumnValues[i]))) + " |";
-            var headerDataDividerLine = "| " + string.Join(" | ", properties.Select((n, i) => new string('-', maxColumnValues[i]))) + " |";
+            var headerDataDividerLine = "| " + string.Join(" | ", gettables.Select((n, i) => new string('-', maxColumnValues[i]))) + " |";
 
             var lines = new[]
                 {
@@ -30,7 +38,7 @@ namespace System.Linq
                 }.Union(
                     source
                     .Select(s =>
-                        "| " + string.Join(" | ", properties.Select((n, i) => (n.GetValue(s)?.ToString() ?? "").PadRight(maxColumnValues[i]))) + " |"));
+                        "| " + string.Join(" | ", gettables.Select((n, i) => (n.GetValue(s)?.ToString() ?? "").PadRight(maxColumnValues[i]))) + " |"));
 
             return lines
                 .Aggregate((p, c) => p + Environment.NewLine + c);
